56ba1a8c62a84c07d2697203671456b048d595b4
Julie <raymondjulie@proton.me>
2024-02-28 14:10:43 +0100

    exercice component quest 2 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlockComponent } from "./block/block.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, BlockComponent]
 })
 export class AppComponent {
-  title = 'myFirstBinding';
+  title = 'myFirstComposant';
 }
